5989187fd7dbb754df4c93966b7b39bd129b8644
Michael Raymond <michael_raymond@fastmail.com>
2024-11-09 13:57:29 +0000

    Abandon llvm

--- lookup.c
+++ lookup.c
@@ -1,54 +1,54 @@
 #include "lookup.h"
-#include "vecs.h"
 #include <immintrin.h>
 #include <stdio.h>
 
-const u_int8_t zeros[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-const u_int8_t fours[16] = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
-const u_int8_t fs[16] = {0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F,
-                         0x0F};
-//
-//// First byte high nibble
-const u_int8_t table1[16] = {1, 1, 1, 1, 1, 1, 1, 1, 128, 128, 128, 128, 6, 2, 26, 98};
-//// First byte low nibble
-const u_int8_t table2[16] = {183, 135, 131, 131, 195, 227, 227, 227, 227, 227, 227, 227, 227, 235, 227, 227};
-//// Second byte high nibble
-const u_int8_t table3[16] = {2, 2, 2, 2, 2, 2, 2, 2, 181, 213, 205, 205, 2, 2, 2, 2};
-
-__m128i classify(__m128i input, __m128i previous_input) {
-    __m128i prev1 = vec_shift_into(previous_input, input);
-    __m128i shift_vec = _mm_load_si128((const __m128i *) fours);
-    __m128i shifted = vec_shr(prev1, shift_vec);
-    __m128i byte_1_high = _mm_shuffle_epi8(_mm_load_si128((const __m128i *) table1), shifted);
-    __m128i masked = vec_bitand(prev1, _mm_load_si128((const __m128i *) fs));
-    __m128i byte_1_low = _mm_shuffle_epi8(_mm_load_si128((const __m128i *) table2), masked);
-    __m128i shifted2 = vec_shr(input, shift_vec);
-    __m128i byte_2_high = _mm_shuffle_epi8(_mm_load_si128((const __m128i *) table3), shifted2);
-
-    return vec_bitand(vec_bitand(byte_1_high, byte_1_low), byte_2_high);
+void print_vec(__m128i v) {
+    char a[16];
+    _mm_store_si128((__m128i *) a, v);
+    for (int i = 0; i < 16; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
 }
-//
-//bool lookup_validate(const char *bytes, size_t len) {
-//    __m128i v0 = _mm_load_si128((const __m128i *) zeros);
-//    __m128i v1 = _mm_load_si128((const __m128i *) bytes);
-//    vec_debug(classify(v1, v0));
-//    return true;
-//}
 
 bool lookup_validate(const char *bytes, size_t len) {
     size_t l = len - (len % 16);
     size_t i = 0;
-    __m128i v0 = _mm_load_si128((const __m128i *) zeros);
+    __m128i v0 = _mm_set1_epi8(0);
+    __m128i shift_vec = _mm_set1_epi8(4);
+    __m128i mask_vec = _mm_set1_epi8(15);
+    // First byte high nibble
+    __m128i table1 = _mm_set_epi8(1, 1, 1, 1, 1, 1, 1, 1, 128, 128, 128, 128, 6, 2, 26, 98);
+    // First byte low nibble
+    __m128i table2 = _mm_set_epi8(183, 135, 131, 131, 195, 227, 227, 227, 227, 227, 227, 227, 227, 235, 227, 227);
+    // Second byte high nibble
+    __m128i table3 = _mm_set_epi8(2, 2, 2, 2, 2, 2, 2, 2, 181, 213, 205, 205, 2, 2, 2, 2);
 
     u_int8_t acc = 0;
 
-    while (i < l) {
-//        __m128i v1 = _mm_load_si128((const __m128i *) bytes + i);
-        __m128i v1 = _mm_load_si128((const __m128i *) zeros);
-        acc = vec_reduce_or(classify(v1, v0));
-        i += 16;
-        v0 = v1;
-    }
+    // tmp
+    __m128i v1 = _mm_load_si128((const __m128i *) &bytes[i]);
+//    __m128i masked = _mm_and_si128(v1, _mm_set1_epi8(240))
+    __m128i shifted = _mm_(_mm_and_si128(v1, _mm_set1_epi8(240), )
+
+    print_vec(v1);
+
+    // tmp
+//    while (i < l) {
+//        __m128i v1 = _mm_load_si128((const __m128i *) &bytes[i]);
+//        __m128i prev1 = _mm_alignr_epi8(v1, v0, 15);
+//        // todo shift!
+//        __m128i byte_1_high = _mm_shuffle_epi8(table1, prev1);
+//        __m128i masked = _mm_and_si128(prev1, mask_vec);
+//        __m128i byte_1_low = _mm_shuffle_epi8(table2, prev1);
+//        // todo shift!
+//        __m128i byte_2_high = _mm_shuffle_epi8(table3, v1);
+//
+//        volatile __m128i anded = _mm_and_si128(_mm_and_si128(byte_1_high, byte_1_low), byte_2_high);
+////        acc = vec_reduce_or(classify(prev1, v0));
+//        i += 16;
+//        v0 = v1;
+//    }
 
     return acc == 0;
 }
